e029fe72939906a742ab886438e30eb0198f07bc
Michelle Sharer <michellesharer@gmail.com>
2024-01-13 12:25:34 -0700

    Quartz sync: Jan 13, 2024, 12:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "michellesh.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
