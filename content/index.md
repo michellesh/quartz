e504cf7f5b52763c74a82bf68b3f649c0747c8a9
Michelle Sharer <michellesharer@gmail.com>
2024-01-13 13:56:40 -0700

    Quartz sync: Jan 13, 2024, 1:56 PM

--- content/index.md
+++ content/index.md
@@ -2,5 +2,7 @@
 title: Welcome to the Rainbow Clock Guide
 ---
 
+![](static/rainbow-clock-photo.png)
+
 This is a blank Quartz installation.
 See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz",
+    pageTitle: "Rainbow Clock Guide",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
